01981b029b56567ff81e1b14e4a5bcebe4c7b001
Trọng Hiếu Nguyễn <anhtien96t@gmail.com>
2026-06-05 08:43:08 +0700

    first commit

---
+++ src/test/java/com/ltweb2/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ltweb2.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
